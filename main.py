de2d7c68da59763b784f143eec856ce9dc07534d
The-soulless12 <aklirayane2@gmail.com>
2025-05-08 18:09:20 +0100

    modification légére de l'affichage

--- main.py
+++ main.py
@@ -6,52 +6,49 @@ from tkinter import messagebox
 
 def creer_taquin(taille):
     while True:
-        # On génère les valeurs dans le désordre
         valeurs = list(range(1, taille * taille)) + [0] 
         random.shuffle(valeurs)
         grille = [valeurs[i * taille:(i + 1) * taille] for i in range(taille)]
         
-        # Vérifie si le taquin est solvable
         if est_solvable(grille) and grille != [[(i * taille + j + 1) % (taille * taille) for j in range(taille)] for i in range(taille)]:
             return grille
 
 def transposition(valeurs, N):
     # Cette fonction calcule le nombre de transpositions dans une configuration donnée
-    valeurs_new = [N if v == 0 else v for v in valeurs]  # On remplace la case vide par N
+    valeurs_new = [N if v == 0 else v for v in valeurs]  
     cible = sorted(valeurs_new)
     transpositions = 0
-    index_map = {val: i for i, val in enumerate(valeurs_new)}  # On cherche la losition actuelle de chaque valeur
+    index_map = {val: i for i, val in enumerate(valeurs_new)}  
     
     for i in range(len(valeurs_new)):
-        while valeurs_new[i] != cible[i]:  # Tant que la valeur à l'index n'est pas correcte
-            cible_index = index_map[cible[i]]  # L'index où la valeur correcte doit être
-            valeurs_new[i], valeurs_new[cible_index] = valeurs_new[cible_index], valeurs_new[i]  # On effectue un swap
-            index_map[valeurs_new[cible_index]] = cible_index  # On met à jour l'index de la valeur échangée
-            index_map[valeurs_new[i]] = i  # On met à jour l'index de la valeur actuelle
-            transpositions += 1  # On augmenter le compteur de transpositions
+        while valeurs_new[i] != cible[i]:  
+            cible_index = index_map[cible[i]]  
+            valeurs_new[i], valeurs_new[cible_index] = valeurs_new[cible_index], valeurs_new[i]  
+            index_map[valeurs_new[cible_index]] = cible_index  
+            index_map[valeurs_new[i]] = i
+            transpositions += 1  
     
     return transpositions 
 
 def permutations_case_vide(valeurs, taille):
     # Cette fonction calcule le nombre de permutations nécessaires pour déplacer la case vide jusqu'à la fin du tableau
     valeurs_new = [taille*taille if v == 0 else v for v in valeurs]
-    index_vide = valeurs_new.index(taille*taille)  # On cherche la position actuelle de la case vide
+    index_vide = valeurs_new.index(taille*taille) 
     
     ligne = (index_vide) // taille
     colonne = (index_vide) % taille
-    # On permute horizontalement la case vide jusqu'à la fin de sa ligne puis verticalement jusqu'à la fin de sa colonne
     return ( 2*taille- 2 - ligne - colonne)
 
 def est_solvable(grille):
     # Cette fonction vérifie si une configuration de taquin est solvable ou pas
     taille = len(grille)
     valeurs = [grille[i][j] for i in range(taille) for j in range(taille)]
-    N = taille * taille  # Valeur à utiliser pour remplacer le 0
+    N = taille * taille 
     
     nb_transposition = transposition(valeurs, N)
     nb_permutations= permutations_case_vide(valeurs, taille)
     # Le problème est solvable si la parité de la permutation est identique à la parité de permutation la case vide
-    return (nb_transposition % 2) == (nb_permutations% 2) # Il s'agit d'un XOR
+    return (nb_transposition % 2) == (nb_permutations% 2)
 
 def afficher_taquin(grille):
     # Cette fonction affiche le taquin en mode console
@@ -64,7 +61,6 @@ def afficher_taquin(grille):
         ligne_str = ""
         for j, cell in enumerate(ligne):
             if (i, j) in cases_cibles(grille):
-                # On affiche en rouge les cases qui peuvent bouger
                 cell_str = f"{cell if cell != 0 else ' ' :>{largeur}}"
                 ligne_str += f"| {Fore.RED}{cell_str}{Style.RESET_ALL} "
             else:
@@ -151,7 +147,6 @@ def a_star(taquin):
     taille = len(taquin)
     objectif = [[(i * taille + j + 1) % (taille * taille) for j in range(taille)] for i in range(taille)] # Il s'agit de notre état final
     
-    # On initialise la liste des nœuds ouverts
     open_list = []
     heapq.heappush(open_list, (heuristique(taquin), taquin, []))  # coût f(n), grille, chemin
     # On initialise la liste des nœuds fermés (les grilles déjà explorées)
@@ -174,13 +169,12 @@ def a_star(taquin):
                 heapq.heappush(open_list, (f_voisin, voisin, chemin_mouvements + [mouvement]))
     return None
 
-# Création de l'interface graphique du Taquin
 class TaquinApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Taquin")
 
-        self.taille = 4 # La taille du taquin
+        self.taille = 3 
         self.taquin = creer_taquin(self.taille)
 
         self.frame = tk.Frame(self.root)
@@ -217,7 +211,6 @@ class TaquinApp:
                                  width=4, height=2, relief="solid", font=("Courier", 14))
                 label.grid(row=i, column=j, padx=5, pady=5)
                 self.labels[i][j] = label
-                # Ajout d'un événement de clic pour déplacer les pièces
                 label.bind("<Button-1>", lambda event, x=i, y=j: self.clic_piece(x, y))
         self.update()
 
@@ -231,10 +224,8 @@ class TaquinApp:
                     self.labels[i][j].config(text=str(value), bg="lightpink")
 
     def clic_piece(self, i, j):
-        # On cherche la position de la case vide
         x, y = trouver_case_vide(self.taquin)
         
-        # On vérifie si la pièce cliquée est adjacente à la case vide
         if abs(i - x) + abs(j - y) == 1:
             self.taquin[x][y], self.taquin[i][j] = self.taquin[i][j], self.taquin[x][y]
             self.update()
@@ -258,22 +249,29 @@ class TaquinApp:
 
     def trouver_solution(self):
         sequence_solution = a_star(self.taquin)
-        if self.solution_fenetre:  # Si la fenêtre de solution est déjà ouverte, il faut la fermer
+        if self.solution_fenetre:  
             self.solution_fenetre.destroy()
         
-        # On crée une nouvelle fenêtre pour afficher la solution
         self.solution_fenetre = tk.Toplevel(self.root)
         self.solution_fenetre.title("Solution")
-        solution_text = "Voici la solution du taquin :\n" + "\n".join(sequence_solution)
-        
-        solution_label = tk.Label(self.solution_fenetre, text=solution_text, font=("Courier", 14))
-        solution_label.pack(padx=20, pady=20)
+        frame_scroll = tk.Frame(self.solution_fenetre)
+        frame_scroll.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
+
+        scrollbar = tk.Scrollbar(frame_scroll)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        text_box = tk.Text(frame_scroll, wrap=tk.WORD, yscrollcommand=scrollbar.set, font=("Courier", 14), height=20, width=50)
+        text_box.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        scrollbar.config(command=text_box.yview)
+
+        text_box.insert(tk.END, "Voici la solution du taquin :\n\n" + "\n".join(sequence_solution))
+        text_box.config(state=tk.DISABLED)
 
         fermer_button = tk.Button(self.solution_fenetre, text="Fermer", command=self.solution_fenetre.destroy)
         fermer_button.pack(pady=10)
 
     def reinitialiser(self):
-        if self.solution_fenetre:  # Si la fenêtre de solution est ouverte, la fermer
+        if self.solution_fenetre:  
             self.solution_fenetre.destroy()
             self.solution_fenetre = None
 
